e8c8f51092a201339235e5c94fe42749f4fdb5f2
同温层 <1998neutrino@gmail.com>
2024-12-06 16:48:17 +0800

    add retry to ddg search tool.

--- tools/search.py
+++ tools/search.py
@@ -1,6 +1,12 @@
 import os
 from langchain_community.utilities.duckduckgo_search import DuckDuckGoSearchAPIWrapper
+from duckduckgo_search.exceptions import RatelimitException
 from langchain_core.tools import tool
+import time
+# Log
+from my_logger import Logger, LOG_LEVEL, LOG_PATH, LOG_FILE
+# Initialize Logger
+logger = Logger(name="AgentLogger", level=LOG_LEVEL, log_path=LOG_PATH, log_file=LOG_FILE)
 
 @tool
 def ddg_search_engine(query: str, workspace: dict = {}) -> list[dict]:
@@ -16,9 +22,22 @@ def ddg_search_engine(query: str, workspace: dict = {}) -> list[dict]:
         [{"url":"page link", "title":"page title", "content":"page content", "date":"page date"}, ...]
     """
     max_results = 5
+    max_retries = 3
+    wait_time = 3 # wait for 1s before retry
     ddg_api = DuckDuckGoSearchAPIWrapper(time='m', source="text") # Time Options: d, w, m, y
-    results = ddg_api.results(query, max_results=max_results)
-    return {"result": [{"url": r.get("link"), "title": r.get("title"), "content": r.get("snippet"), "date": r.get("date", "")} for r in results]}
+    retries = 0
+    while retries < max_retries:
+        try:
+            results = ddg_api.results(query, max_results=max_results)
+            return {"result": [{"url": r.get("link"), "title": r.get("title"), "content": r.get("snippet"), "date": r.get("date", "")} for r in results]}
+        except RatelimitException:
+            retries += 1
+            logger.logger.warning((f"Retrying search for '{query}' due to rate limits... (Attempt {retries})"))
+            time.sleep(wait_time)  # Wait before retryin
+    logger.logger.warning((f"Failed to search for '{query}' after {max_retries} retries."))
+    return  {"result": f"Failed to search for '{query}' due to API rate limit. Please try again."}
+
+    
 
 if __name__ == "__main__":
     import asyncio
