ff0681e9b7dad341cdb0f10e8df0386796d1053d
同温层 <1998neutrino@gmail.com>
2025-01-03 14:23:06 +0800

    add requirement to installation

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
+def load_requirements(filename="requirements.txt"):
+    with open(filename, "r", encoding="utf-8") as f:
+        return f.read().splitlines()
+
 setup(
-    name='agentic_task_solver',  # 包名
+    name='agentic_task_solver',  
     version='0.1.0',
-    packages=find_packages(),  # 自动发现所有的包
-    install_requires=[  # 如果有其他依赖，列在这里
-        # 'some_package',
-    ],
-    # 可以添加其他设置，比如作者信息等
+    packages=find_packages(),  
+    install_requires=load_requirements(), 
     author='同温层',
     author_email='1998neutrino@gmail.com',
     description='A multi-agent system designed to collectively solve a given task based on langchain & langgraph.',
